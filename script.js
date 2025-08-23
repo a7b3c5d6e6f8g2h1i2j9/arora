2a384642f14ed1b2b635ce77a54c16ad9e30675c
a7b3c5d6e6f8g2h1i2j9 <anirudhananu592@gmail.com>
2025-08-23 12:07:20 +0530

    file

--- script.js
+++ script.js
@@ -172,28 +172,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 
-document.querySelector("#myForm").addEventListener("submit", async (e) => {
-  e.preventDefault();
 
-  const formData = {
-    name: document.querySelector("#name").value,
-    Phone: document.querySelector("#phone").value,
-    email: document.querySelector("#email").value,
-    message: document.querySelector("#message").value
-  };
-
-  const response = await fetch("https://arora-backend.onrender.com/sign_up", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(formData)
-  });
-
-  const result = await response.json();
-  if (result.success) {
-    window.location.href = "/signup_successfull.html"; // redirect client-side
-  } else {
-    alert("Error: " + result.message);
-  }
-});
 
 
